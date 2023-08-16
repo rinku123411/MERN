87d52f41945c10bf1335e29987f986d303ee9fee
rinku123411 <rinkudas123411@gmail.com>
2023-08-16 23:50:53 +0530

    added collections

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,17 +1,19 @@
 const express= require('express')
+const{
+    createWorkout,
+    getWorkouts,
+    getWorkout
+
+} =require('../controllers/workoutController')
 const router= express.Router()
 //GET all workouts
-router.get('/',(req,res)=>{
-    res.json({mssg: 'GET all workouts'})
-})
+router.get('/',getWorkouts)
+
 //GET single workout
-router.get('/:id',(req,res)=>{
-    res.json({mssg: 'GET single workout'})
-})
+router.get('/:id',getWorkout)
 //POST a new workout
-router.post('/',(req,res)=>{
-    res.json({mssg:'POST a new workout'})
-})
+router.post('/', createWorkout)
+
 //DELETE a workout
 router.delete('/:id',(req,res)=>{
     res.json({mssg: 'DELETE a workout'})
